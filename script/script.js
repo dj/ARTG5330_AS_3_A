17ac618b049032fceb60163adc4cf7a2e9e0e84a
DJ Hartman <danieljayhartman@gmail.com>
2015-03-18 09:55:22 -0400

    Draw top 5 on button click

--- script/script.js
+++ script/script.js
@@ -13,57 +13,60 @@ var canvas = d3.select('.plot')
 //Scale for the size of the circles
 var scaleR = d3.scale.sqrt().domain([5,100]).range([5,120]);
 
-
 d3.csv('data/olympic_medal_count.csv', parse, dataLoaded);
 
 function dataLoaded(err,rows){
+  var top1900 = top5(rows, 1900);
 
-    var year = 1900;
-    rows.sort(function(a,b){
-        //Note: this is called a "comparator" function
-        //which makes sure that the array is sorted from highest to lowest
-        return b[year] - a[year];
-    });
+  draw(top1900, 1900);
 
-    //Note: this returns positions 0,1,2,3,4 of the "rows" array
-    var top5 = rows.slice(0,5);
-    draw(top5, year);
+  //TODO: fill out this function
+  $('.btn-group .year').on('click',function(e){
+    e.preventDefault();
 
-    //TODO: fill out this function
-    $('.btn-group .year').on('click',function(e){
-        e.preventDefault();
+    var year = $(this).data('year');
 
-        var year = $(this).data('year');
-        console.log(year);
-    });
+    var topTeams = top5(rows, year)
+
+    draw(topTeams, year);
+  });
+}
+
+function top5(rows, year) {
+  var sorted = _.sortBy(rows, year).reverse(),
+      result = sorted.slice(0,5);
+
+  console.log(year.toString() + ': ' + _.pluck(result, year))
+
+  return result;
 }
 
 function draw(rows, year){
-    var topTeams = canvas.selectAll('.team')
-        .data(rows, function(d){ return d.country; })
-        .enter()
-        .append('g')
-        .attr('class', 'team')
-        .attr('transform',function(d,i){
-            //i ranges from 0 to 4
-            return 'translate(' + i*(width/4) + ',' + height/2 + ')';
-        });
+  var topTeams = canvas.selectAll('.team')
+    .data(rows, function(d){ return d.country; })
+    .enter()
+    .append('g')
+    .attr('class', 'team')
+    .attr('transform',function(d,i){
+      //i ranges from 0 to 4
+      return 'translate(' + i*(width/4) + ',' + height/2 + ')';
+    });
+  topTeams
+    .append('circle')
+    .attr('r', function(d){
+      return scaleR(d[year]);
+    })
+  topTeams
+    .append('text')
+    .attr('class','team-name')
+    .text(function(d){ return d.country; })
+    .attr('y', function(d){ return scaleR(d[year]+20)})
+    .attr('text-anchor','middle');
     topTeams
-        .append('circle')
-        .attr('r', function(d){
-            return scaleR(d[year]);
-        })
-    topTeams
-        .append('text')
-        .attr('class','team-name')
-        .text(function(d){ return d.country; })
-        .attr('y', function(d){ return scaleR(d[year]+20)})
-        .attr('text-anchor','middle');
-    topTeams
-        .append('text')
-        .attr('class','medal-count')
-        .text(function(d){ return d[year];})
-        .attr('text-anchor','middle');
+    .append('text')
+    .attr('class','medal-count')
+    .text(function(d){ return d[year];})
+    .attr('text-anchor','middle');
 }
 
 function parse(row){
